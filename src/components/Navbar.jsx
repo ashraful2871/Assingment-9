c1ebe46a3305ee70ca178a99436b9d1f057f9f7e
ashraful2871 <ashrafulislam2758@gmail.com>
2024-11-19 18:31:02 +0600

    change web name ancor tag to ling tag and it is dynamic when click any route then redirect to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,9 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-3xl">Collect Coupon </a>
+        <Link to="/" className="font-bold text-3xl">
+          Collect Coupon{" "}
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
